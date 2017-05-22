ef2f2531f45bfba332275a9ce5e3c19ad1dba211
Luke Persola <lukepersola@gmail.com>
2017-05-22 14:30:10 -0700

    fuck JSX files

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
