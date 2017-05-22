2084d189ada5a386b3ad9de2b90d1c232108fcbd
Luke Persola <lukepersola@gmail.com>
2017-05-22 12:55:05 -0700

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+// let redux = require('redux')
+
+let e = 1;
+var j = 'asdasdk';
+console.log('COOKIES MONSTER RULEZ THE SKOOLZ');
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: (__dirname + '/build/'),
+    filename: 'compiled_app.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
