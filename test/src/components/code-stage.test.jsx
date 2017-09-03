14ba7e35ab011b61af2b280bd6fc4d33842f5eb2
Luke Persola <lukepersola@gmail.com>
2017-09-02 20:22:31 -0700

    add SyntacticalNode tests

--- test/src/components/code-stage.test.jsx
+++ test/src/components/code-stage.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { CodeStage } from '../src/components/code-stage.jsx';
+import { CodeStage } from '../../../src/components/code-stage.jsx';
 
 describe ('CodeStage', () => {
   it ('renders', () => {
---
+++ test/src/components/syntactical-node.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SyntacticalNode from '../../../src/components/syntactical-node.jsx';
+
+describe ('SyntacticalNode', () => {
+  const validProps = {
+    serialization: {
+      klass: 'numberLiteral',
+      data: 678,
+    }
+  }
+
+  it ('renders', () => {
+    const { serialization } = validProps
+
+    expect(shallow(
+      <SyntacticalNode serialization={serialization} />
+    )).toMatchSnapshot();
+  });
+
+  it ('only works for numberLiterals', () => {
+    const serialization = Object.assign({}, validProps.serialization, {
+      klass: 'otherKlass',
+    })
+
+    expect( () => {
+      shallow(
+        <SyntacticalNode serialization={serialization} />
+      )
+    }).toThrow('no types yet');
+  });
+})
